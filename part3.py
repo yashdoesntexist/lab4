a8087f48369a95aab662e80e2a145e814be012ed
agentP <learnhacksandtricks@gmail.com>
2025-11-06 12:08:31 -0800

    question

--- part3.py
+++ part3.py
@@ -22,5 +22,10 @@ partitioned_result = partitioned.reduceByKey(lambda a, b: a + b)
 
 print(partitioned_result.take(10))
 
+#Custom partitioning changes workload balance by letting us control how keys are assigned to partitions instead of relying on Spark’s default distribution.
+#  When we choose a partitioning rule such as grouping words by their first letter, we can spread the data more evenly across partitions. 
+# This prevents one partition from receiving too many records and becoming a bottleneck.
+#  As a result, work is distributed more evenly, and overall job performance improves.
+
 #Q2
 #Custom partitioning lets you control where keys go, which can improve load balancing if the key distribution matches your partition logic.
